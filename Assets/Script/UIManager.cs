9eaee22c4be36a7b3f46b7cc6a8583aab9d5f3d2
young <pink495sy@gmail.com>
2023-03-23 20:17:16 +0900

    시작화면 UI

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -27,8 +27,8 @@ public class UIManager : MonoBehaviour
     {
         if(GameManager.Instance.timerOn)
         {
-            min.text = GameManager.Instance.min.ToString();
-            sec.text = GameManager.Instance.sec.ToString();
+//            min.text = GameManager.Instance.min.ToString();
+ //           sec.text = GameManager.Instance.sec.ToString();
         }
     }
 }
