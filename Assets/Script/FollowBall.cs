4a2737d4642e28a3e6841bca89c887b3ddc696f2
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-03-27 15:38:41 +0900

    카메라 뷰 각도 수정

--- Assets/Script/FollowBall.cs
+++ Assets/Script/FollowBall.cs
@@ -9,7 +9,7 @@ public class FollowBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = ball.transform.position + new Vector3(1.44f, 14f, 18.3f);
+        transform.position = ball.transform.position + new Vector3(1.44f, 14f, 27.57f);
         //transform.rotation = Quaternion.Euler(transform.rotation.x, 180f+ball.rotation.y, transform.rotation.z);
     }
 }
